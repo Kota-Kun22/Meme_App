4eb76436a74c9df240e91303bda00b9bef75a2d3
Harsh Rastogi <harshrastogi013@gmail.com>
2024-03-28 13:30:37 +0530

    Utils package is done OBJECTS are made that are Retrofit_Instance.kt is completed along with Utils object

--- app/src/main/java/com/example/meme_app/Utils/Retrofit_Instance.kt
+++ app/src/main/java/com/example/meme_app/Utils/Retrofit_Instance.kt
@@ -1,4 +1,17 @@
 package com.example.meme_app.Utils
 
+import com.example.meme_app.Data.Api_Interface
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object Retrofit_Instance {
+    val api: Api_Interface by lazy {
+        Retrofit.Builder()
+            .baseUrl(Utils.BASEURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(Api_Interface::class.java)
+
+    }
 }
--- app/src/main/java/com/example/meme_app/Utils/Utils.kt
+++ app/src/main/java/com/example/meme_app/Utils/Utils.kt
@@ -1,4 +1,5 @@
 package com.example.meme_app.Utils
 
 object Utils {
+    const val BASEURL= "https://api.imgflip.com/"
 }
