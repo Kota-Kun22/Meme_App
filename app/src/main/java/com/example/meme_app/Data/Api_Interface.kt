a471131c5dc5d5e35ccd99c0769663ccc17b89be
Harsh Rastogi <harshrastogi013@gmail.com>
2024-03-28 23:05:00 +0530

    retrofit is all done model are done data classes are done

--- app/src/main/java/com/example/meme_app/Data/Api_Interface.kt
+++ app/src/main/java/com/example/meme_app/Data/Api_Interface.kt
@@ -1,4 +1,9 @@
 package com.example.meme_app.Data
 
-interface Api_INterface {
+import com.example.meme_app.Models.Meme
+import retrofit2.http.GET
+
+interface Api_Interface {
+    @GET("get_memes")
+    suspend fun getALLMemesList() :Result<Meme>
 }
--- app/src/main/java/com/example/meme_app/Utils/Utils.kt
+++ app/src/main/java/com/example/meme_app/Utils/Utils.kt
@@ -1,4 +1,5 @@
 package com.example.meme_app.Utils
+// https://api.imgflip.com/get_memes
 
 object Utils {
     const val BASEURL= "https://api.imgflip.com/"
